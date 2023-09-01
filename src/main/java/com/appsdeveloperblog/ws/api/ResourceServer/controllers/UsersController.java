0cd0ea2fb40df9dea92f8ae5cdb0f7bfaa2b6209
Albert Manukyan <albertmanukyan975@gmail.com>
2023-09-01 14:26:57 +0400

    commit

--- src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,7 +1,8 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
 import com.appsdeveloperblog.ws.api.ResourceServer.dto.UserRest;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,20 +13,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working ...";
+        return "Working on the port " + environment.getProperty("local.server.port");
     }
 
-//    @Secured("ROLE_developer")
+    //    @Secured("ROLE_developer")
     @PreAuthorize("hasRole('developer') or #id == #jwt.subject")
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
         return "Deleted user with id  " + id;
     }
+
     @PostAuthorize("returnObject.userId == #jwt.subject")
     @GetMapping(path = "/{id}")
     public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
-        return new UserRest("Albert" , "Manukyan" , "");
+        return new UserRest("Albert", "Manukyan", "");
     }
 }
